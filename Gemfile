fcdeebf1d70f79e36f80d48fd642e40965de473c
tsuchida031025 <tuusan33@gmail.com>
2019-10-31 14:12:19 +0900

    add controllerUsers
    ユーザー管理機能を追加

--- Gemfile
+++ Gemfile
@@ -58,3 +58,6 @@ gem 'haml-rails'
 
 #font-awesomeの導入
 gem "font-awesome-rails"
+
+#deviseの導入
+gem 'devise'
--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,7 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
-    end  end
+      config.i18n.default_locale = :ja
+    end
+  end
 end
