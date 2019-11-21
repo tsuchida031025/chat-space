afb78a55ddcef634837c74639a885b315d9ade58
tsuchida031025 <tuusan33@gmail.com>
2019-11-21 17:12:59 +0900

    remove_UnnecessaryCode

--- app/controllers/api/messages.controller.rb
+++ app/controllers/api/messages.controller.rb
@@ -1,5 +1,3 @@
-app/controllers/api/messages_controller.rb
-
 class Api::MessagesController < ApplicationController
   def index
     # ルーティングでの設定によりparamsの中にgroup_idというキーでグループのidが入るので、これを元にDBからグループを取得する
