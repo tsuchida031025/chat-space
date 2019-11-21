88a7777e6bca30a6478a8e8feea20e86200b2e87
tsuchida031025 <tuusan33@gmail.com>
2019-11-21 14:34:22 +0900

    指摘事項の反映（if文を三項演算子による記述に変更）

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,58 +1,24 @@
 $(document).on('turbolinks:load',function(){
   function buildHTML(message){
     var Messageimage = (message.image)? (message.image) : ""
-    if(message.content && Messageimage){
-      var html =
-        `<div class="message" data-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
+    var html =
+      `<div class="message" data-id=${message.id}>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
           </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
+          <div class="upper-message__date">
+            ${message.created_at}
           </div>
-          <img src=${Messageimage} >
-        </div>`
-    } else if (message.content){
-      var html =
-        `<div class="message" data-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-    } else if (Messageimage){
-      var html =
-        `<div class="message" data-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <img src=${Messageimage} >
-          </div>
-        </div>`
-    };
-    return html;
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+        <img src=${Messageimage} >
+      </div>`
+    return (message.content || Messageimage) ? html : '';
   }
   $('.new_message').on('submit', function(e){
     e.preventDefault()
