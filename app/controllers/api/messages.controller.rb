c95eb82d9d92a4b64f5251855752963928504963
tsuchida031025 <tuusan33@gmail.com>
2019-11-21 17:28:50 +0900

    modify_blankSpace

--- app/controllers/api/messages.controller.rb
+++ app/controllers/api/messages.controller.rb
@@ -5,7 +5,7 @@ class Api::MessagesController < ApplicationController
     # ajaxで送られてくる最後のメッセージのid番号を変数に代入
     last_message_id = params[:id].to_i
     # 取得したグループでのメッセージ達から、idがlast_message_idよりも新しい(大きい)メッセージ達のみを取得
-    @messages = group.messages.includes(:user).where("id > #{last_message_id}")
+    @messages = group.messages.includes(:user).where("id > #{last_message_id}")
     end
   end
 end
