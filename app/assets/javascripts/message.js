04a86f3e74f902c2dcc4fc555eee489b9ca4c214
tsuchida031025 <tuusan33@gmail.com>
2019-11-22 18:58:23 +0900

    指摘事項の反映

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,10 @@
-$(document).on('turbolinks:load',function(){
+//$(document).on('turbolinks:load',function(){
+$(function(){
+
   function buildHTML(message){
     var Messageimage = (message.image)? (message.image) : ""
     var html =
-      `<div class="message" data-id=${message.id}>
+      `<div class="message" data-message-id=${message.id}>
         <div class="upper-message">
           <div class="upper-message__user-name">
             ${message.user_name}
@@ -44,27 +46,26 @@ $(document).on('turbolinks:load',function(){
     })
   })
   var roloadMessages = function(){
-      if (window.location.href.match(/\/groups\/\d+\/messages/)){
-        var last_message_id = $('.message:last').data("message-id");
-        $.ajax({
-          url: 'api/messages',
-          type: 'get',
-          dataType: 'json',
-          data: {id: last_message_id}
-        })
-        .done(function(messages) {
-          var insertHTML = ''; 
-          messages.forEach(function (message) { 
-          insertHTML = buildHTML(message); 
-          $('.messages').append(insertHTML); 
-  
-          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-          })
-        })
-        .fail(function() {
-          alert('自動更新に失敗しました');
-        });
-      }
-    };
-    setInterval(roloadMessages, 7000);
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message-id");
+      $.ajax({
+        url: 'api/messages',
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML = ''; 
+        messages.forEach(function (message) { 
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        })
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(roloadMessages, 2000);
 });
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :messages, only: [:index, :create]
 
     namespace :api do
-      resources :messages, only: :index, defaults: { format: 'json '}
+      resources :messages, only: :index, defaults: { format: 'json'}
     end
   end
 end
